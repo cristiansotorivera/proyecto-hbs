f26d4c8a30fa9f79a056a957fea67a31dafdffd8
Cristian Soto <cristiansotorivera@gmail.com>
2024-04-20 11:48:49 -0400

    update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { engine } from 'express-handlebars';
 import path from 'path'
 
+import { services } from "./data/services.data.js"
+
 const app = express()
 
 // ruta absoluta
@@ -38,18 +40,6 @@ app.get('/services/:service', (req, res) => {
   });
   
 
-const products = [
-    { id: 1, title: "ZTE", img: "https://www.pcfactory.cl/public/foto/50037/1_200.jpg?t=1713542184599" },
-    { id: 2, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/48183/1_200.jpg?t=1712348209323" },
-    { id: 3, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/44473/1_200.jpg?t=1709222667989" },
-    { id: 4, title: "Xiaomi", img: "https://www.pcfactory.cl/public/foto/50697/1_200.jpg?t=1712349941174" },
-]
-
-const fruts = ['🍉', '🍓', '🍌']
-
-app.get('/products', (req, res) => {
-    res.render('products', { products: products, fruts })
-})
 
 
 
